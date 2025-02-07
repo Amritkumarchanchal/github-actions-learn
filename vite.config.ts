dd546d0df1a2f8e4166b33acbcec34ab381e3510
Amrit Kumar Chanchal <amritkrchanchal@gmail.com>
2025-02-07 17:23:59 +0530

    sc
    c

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    outDir: 'dist',  // Confirm this is set, or set it appropriately
+  },
   plugins: [react()],
   resolve: {
     alias: {
